c047b17185ff44a4ab474459e19d4baafe3bd81e
adtvnus <145543633+AdityaNugrahaPS@users.noreply.github.com>
2024-12-09 13:40:22 +0700

    baru lagi

--- header.php
+++ header.php
@@ -1,30 +1,25 @@
-<?php
-// header.php
-
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutomataSE - Pencarian</title>
 
-    <!-- External Libraries -->
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js"></script>
-    <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.js"></script>
-    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <!-- External Libraries (Lokal) -->
+    <link href="CSS/tailwind.css" rel="stylesheet"> <!-- Tailwind CSS -->
+    <script src="JS/gsap.min.js"></script> <!-- GSAP -->
+    <script src="JS/lucide.js"></script> <!-- Lucide -->
+    <script src="JS/particles.min.js"></script> <!-- Particles.js -->
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- CSS Libraries (Lokal) -->
+    <link rel="stylesheet" href="CSS/bootstrap.min.css"> <!-- Bootstrap -->
+    <link rel="stylesheet" href="CSS/font-awesome.css"> <!-- Font Awesome -->
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="CSS/styles.css">
 
-    <!-- External Libraries -->
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <!-- External Libraries (Lokal) -->
+    <link href="CSS/aos.css" rel="stylesheet"> <!-- AOS -->
+    <script src="JS/aos.js"></script> <!-- AOS -->
 </head>
