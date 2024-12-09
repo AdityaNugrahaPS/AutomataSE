0e114dafffdd4ce2eb1f39a3362dff31497f7e33
adtvnus <145543633+AdityaNugrahaPS@users.noreply.github.com>
2024-12-09 15:29:09 +0700

    update

--- header.php
+++ header.php
@@ -1,25 +1,27 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AutomataSE - Pencarian</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AutomataSE - Pencarian</title>
 
-    <!-- External Libraries (Lokal) -->
-    <link href="CSS/tailwind.css" rel="stylesheet"> <!-- Tailwind CSS -->
-    <script src="JS/gsap.min.js"></script> <!-- GSAP -->
-    <script src="JS/lucide.js"></script> <!-- Lucide -->
-    <script src="JS/particles.min.js"></script> <!-- Particles.js -->
+<!-- External Libraries (Lokal) -->
+<link href="CSS/tailwind.css" rel="stylesheet"> <!-- Tailwind CSS -->
+<script src="JS/gsap.min.js"></script> <!-- GSAP -->
+<script src="JS/lucide.js"></script> <!-- Lucide -->
+<script src="JS/particles.min.js"></script> <!-- Particles.js -->
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+<!-- Typed.js Library (Local) -->
+<script src="JS/typed.min.js"></script>
 
-    <!-- CSS Libraries (Lokal) -->
-    <link rel="stylesheet" href="CSS/bootstrap.min.css"> <!-- Bootstrap -->
-    <link rel="stylesheet" href="CSS/font-awesome.css"> <!-- Font Awesome -->
+<!-- Fonts -->
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="CSS/styles.css">
+<!-- CSS Libraries (Lokal) -->
+<link rel="stylesheet" href="CSS/bootstrap.min.css"> <!-- Bootstrap -->
+<link rel="stylesheet" href="CSS/font-awesome.css"> <!-- Font Awesome -->
 
-    <!-- External Libraries (Lokal) -->
-    <link href="CSS/aos.css" rel="stylesheet"> <!-- AOS -->
-    <script src="JS/aos.js"></script> <!-- AOS -->
-</head>
+<!-- Custom CSS -->
+<link rel="stylesheet" href="CSS/styles.css">
+
+<!-- External Libraries (Lokal) -->
+<link href="CSS/aos.css" rel="stylesheet"> <!-- AOS -->
+<script src="JS/aos.js"></script> <!-- AOS -->
